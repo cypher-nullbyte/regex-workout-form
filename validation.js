b6ea09a3d2abe9022a2c678c6f55b5d9d9970552
cYpHeR <github.com/cypher-nullbyte>
2021-03-30 23:23:43 +0530

    regex on telephone

--- validation.js
+++ validation.js
@@ -1,3 +1,27 @@
 // validation script here
 
+const inputs=document.querySelectorAll('input');
 
+const patterns={
+    telephone:/^\d{11}$/
+};
+// validation function
+function validate(field,regex){
+    if(regex.test(field.value))
+    {
+        field.clasName='valid';
+    }
+    else
+    {
+        field.clasName="invalid"
+    }
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup',(e)=>{
+        // console.log(e.target.value);
+        // console.log(e.target.attributes.name.value);
+        // validate(e.target,patterns['telephone']);
+        validate(e.target,patterns[e.target.attributes.name.value]);
+    });
+}); 
